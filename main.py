a044d75b8ffb8b932ee2b08759e6c1f29dc743c1
Bashy <petitpierre@duck.com>
2024-03-18 15:52:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,23 +5,28 @@
 #│ |____/ \__, |  \___/ \___/|_| |_|\__,_|___/____/_____|
 #│        |___/              petitpierre@duck.com
 #╰──────────────────────────────────────────────────────╼
+#import
 import sys
-from src.Utils.update import check_update
 import subprocess
-import src.Main.systek import clear_screen
-import src.Utils.installer import install_systek
+from src.Utils.update import check_update
+from src.Utils.installer import install_systek
+from src.Main.systek import clear_screen
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--update":
+            check_update()
+            sys.exit(0)
+        elif sys.argv[1] == "--install":
+            install_systek()
+            return
+    try:
+        subprocess.run(['sudo', 'systek'], check=True)
+    except subprocess.CalledProcessError as e:
+        clear_screen()
+        print("│ The systek command is not available. Make sure you are running the script on a compatible system. "), e
+        input("╰─╼ Press Enter to continue...")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "--update":
-        check_update()
-        sys.exit(0)
-    elif len(sys.argv) > 1 and sys.argv[1] == "--install":
-        install_systek()
-    else:
-        try:
-            subprocess.run(['sudo', 'systek'], check=True)
-        except subprocess.CalledProcessError as e:
-            clear_screen()
-            print("│ The systek command is not available. Make sure you are running the script on a compatible system. "), e
-            input("╰─╼ Press Enter to continue...")
-            sys.exit(1)
+    main()
