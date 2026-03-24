010091f662ba5d32c2e01aa82845cc9d77d0d02e
Bashy <83141023+jonas52@users.noreply.github.com>
2026-03-24 18:19:19 +0100

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-if [[ "${EUID}" -ne 0 ]]; then
-  echo "Ce script doit être lancé avec sudo/root."
-  exit 1
-fi
-rm -f /usr/local/bin/systek
-rm -rf /opt/systek
-echo "Systek supprimé."
